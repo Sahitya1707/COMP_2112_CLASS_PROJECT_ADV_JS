77a7a3a0219a0c985f7980a08752050f8f049c21
Sahitya hp <neupanesahitya1@gmail.com>
2024-09-10 13:35:11 -0400

    installed jquery and style

--- first_app/js/app.js
+++ first_app/js/app.js
@@ -4,7 +4,37 @@
     console.log("App Started");
     let x = 1;
     console.log(x);
+
+    // fetch & show contacts
+    getContacts((data) => {
+      let list = document.getElementById("contactList");
+      // create a list items for each contact
+      data.forEach((contact) => {
+        let listItem = document.createElement("li");
+        listItem.innerText = contact.Name;
+        listItem.className = "list-group-item";
+        list.appendChild(listItem);
+      });
+    });
   };
 
   window.addEventListener("load", Start);
 })();
+
+let updateCounter = (() => {
+  // now counter gets created and initiialized to zero only once
+  let counter = 0;
+  return () => {
+    // every click can use the same counter var now
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+  };
+})();
+
+let getContacts = (callback) => {
+  // use jquery to read then display our json file contents
+  // the data param gets filled once all the data is read from the file
+  $.getJSON("./data/contact.json", (data) => {
+    callback(data);
+  });
+};
